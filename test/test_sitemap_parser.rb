9c9e873bfa83c656b7ee4655e2e75f14e109a4f1
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2016-04-22 14:12:25 +0100

    Stub tests so that they work offline
    Rather than having to download files over the network we can stub
    Typhoeus so that the tests work without an internet connection.

--- test/test_sitemap_parser.rb
+++ test/test_sitemap_parser.rb
@@ -1,12 +1,17 @@
 require File.join(File.dirname(__FILE__), 'helper')
 
+require 'typhoeus'
+
 class TestSitemapParser < Test::Unit::TestCase
   def setup
-    @url = "https://raw.githubusercontent.com/benbalter/sitemap-parser/master/test/fixtures/sitemap.xml"
-    @sitemap = SitemapParser.new @url
+    url = "https://example.com/sitemap.xml"
+    local_file = File.join(File.dirname(__FILE__), 'fixtures', 'sitemap.xml')
 
-    @local_file = File.join(File.dirname(__FILE__), 'fixtures', 'sitemap.xml')
-    @local_sitemap = SitemapParser.new @local_file
+    response = Typhoeus::Response.new(code: 200, body: File.read(local_file))
+    Typhoeus.stub(url).and_return(response)
+
+    @sitemap = SitemapParser.new url
+    @local_sitemap = SitemapParser.new local_file
 
     @expected_count = 3
   end
@@ -31,14 +36,22 @@ class TestSitemapParser < Test::Unit::TestCase
   end
 
   def test_404
-    sitemap = SitemapParser.new "http://ben.balter.com/foo/bar/sitemap.xml"
+    url = 'http://ben.balter.com/foo/bar/sitemap.xml'
+    response = Typhoeus::Response.new(code: 404, body: "404")
+    Typhoeus.stub(url).and_return(response)
+
+    sitemap = SitemapParser.new url
     assert_nothing_raised do
       assert_equal nil, sitemap.urls
     end
   end
 
   def test_malformed_sitemap
-    url = "https://raw.githubusercontent.com/benbalter/sitemap-parser/master/test/fixtures/malformed_sitemap.xml"
+    url = 'https://example.com/bad/sitemap.xml'
+    malformed_sitemap = File.join(File.dirname(__FILE__), 'fixtures', 'malformed_sitemap.xml')
+    response = Typhoeus::Response.new(code: 200, body: File.read(malformed_sitemap))
+    Typhoeus.stub(url).and_return(response)
+
     sitemap = SitemapParser.new url
     assert_nothing_raised do
       assert_equal [], sitemap.to_a
